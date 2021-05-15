73ccc552bcf07f6862def91d001fbd46181b0a9d
szpeter80 <szpeter80@github.com>
2021-05-15 22:31:08 +0200

    ~ List the drive starting from a given item and print what is non-NFC

--- shot3.py
+++ shot3.py
@@ -4,25 +4,36 @@ import oauth2client.client
 from pydrive.auth  import GoogleAuth,RefreshError
 from pydrive.drive import GoogleDrive
 
+#q_rootfiles = "'root' in parents and trashed = false"
+q_rootfiles = "title contains '---accent_test---'  and trashed = false"
 
-fileStash = {}
-childList = {}
+fileStash = {} # id -> GoogleDriveFile, a dict of all items received from the Drive account
+childList = {} # id -> list of child node id's
+nfcStatus = {} # id -> Boolean; True if name passes as NFC, False if not
+nonNfcItems = {} # id -> path; list of file ids and corresponding full names with path, which are not NFC compliant. These are the elements with nfcStatus->False.
 
+p_printTree = False # Print the directory tree
+
+#'mimeType': 'application/vnd.google-apps.folder'
 def is_gfolder(gdfv2):
   return ('mimeType' in gdfv2 and gdfv2['mimeType']=='application/vnd.google-apps.folder')
   
 def print_gfile(ident_str, gdfv2):
-  prefix = '<DIR> ' if is_gfolder(gdfv2) else ''
-  suffix = ' ***' if not unicodedata.is_normalized('NFC',gdfv2['title']) else ''
-  
-  #fn_nfc = unicodedata.normalize('NFC', file['title'])
+  isdir_str = '<DIR>' if is_gfolder(gdfv2) else ''
+  isnonnfc_str = '' if nfcStatus[gdfv2['id']] else '***'
 
-  print('{}{:<45}{}'.format(ident_str, prefix+gdfv2['title'],suffix))  
+  print('{}{}{:<45}'.format(isnonnfc_str,ident_str, isdir_str+' '+gdfv2['title']+' '+isdir_str))  
 
-#'mimeType': 'application/vnd.google-apps.folder'
 def proc_item(level, ppath, gdfv2):
+  if unicodedata.is_normalized('NFC',gdfv2['title']):
+    nfcStatus[gdfv2['id']] = True
+  else:
+    nfcStatus[gdfv2['id']] = False
+    nonNfcItems[gdfv2['id']] = ppath + gdfv2['title']
+
   ident_str = '{1:>{0}} '.format(4*level, '')
-  print_gfile(ident_str, gdfv2)
+  if p_printTree:
+    print_gfile(ident_str, gdfv2)
 
   if (is_gfolder(gdfv2) and gdfv2['id'] in childList):
     for child_id in childList[gdfv2['id']]:
@@ -40,20 +51,18 @@ if gauth.credentials is None:
 
 gauth.SaveCredentials()
 
-##gauth.LocalWebserverAuth()
-
 drive = GoogleDrive(gauth)
 
 # View all folders and file in your Google Drive
 # PyDrive uses V2 of the Google Drive API  (2021.04.28)
 try:
-  print("Requesting list of all files in this drive ... ", end='', flush=True)
+  print("Requesting list of all files and folders in this drive ... ", end='', flush=True)
   allFileList = drive.ListFile({'q': "trashed = false"}).GetList()
   print("done, {0} items".format(len(allFileList)), flush=True)
 
-  print("Requesting elements in the root folder... ", end='', flush=True)
-  rootFileList = drive.ListFile({'q': "'root' in parents and trashed = false"}).GetList()
-  print("done, {0} items in the root directory\n".format(len(rootFileList)), flush=True)
+  print("Requesting elements to start with... ", end='', flush=True)
+  rootFileList = drive.ListFile({'q': q_rootfiles}).GetList()
+  print("done, {0} items\n".format(len(rootFileList)), flush=True)
 
 except RefreshError:
   print("ERROR: Failed to re-use the access token, probably it has expired or invalid")
@@ -76,6 +85,11 @@ for file in allFileList:
 del allFileList
 
 for file in rootFileList:
-  #print(file)
   proc_item(0, '/', file)
 
+print('\n\n{} item has non-NFC name:\n'.format(len(nonNfcItems)))
+for id,path in nonNfcItems.items():
+  dir_str = ' <DIR>' if is_gfolder(fileStash[id]) else ''
+  print('{}{}'.format(path,dir_str))
+
+#fn_nfc = unicodedata.normalize('NFC', file['title'])
