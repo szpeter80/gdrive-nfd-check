1f170fe3a4601a27f4fa74151f717186b57048d0
szpeter80 <szpeter80@github.com>
2021-05-16 21:57:06 +0200

    + First functional version with parameters and help

--- gdrive-nfc-check.py
+++ gdrive-nfc-check.py
@@ -1,21 +1,25 @@
 # Relevant info about the file attributes can be found here:
 # https://developers.google.com/drive/api/v2/reference/files#resource-representations
 
-import os,sys,resource,unicodedata
+import os,sys,resource,argparse,unicodedata
 import oauth2client.client
 
 from pydrive.auth  import GoogleAuth,RefreshError
 from pydrive.drive import GoogleDrive
-
-#q_rootfiles = "'root' in parents and trashed = false"
-q_rootfiles = "title contains '---accent_test---'  and trashed = false"
 
 fileStash = {} # id -> GoogleDriveFile, a dict of all items received from the Drive account
 childList = {} # id -> list of child node id's
 nfcStatus = {} # id -> Boolean; True if name passes as NFC, False if not
 nonNfcItems = {} # id -> path; list of file ids and corresponding full names with path, which are not NFC compliant. These are the elements with nfcStatus->False.
 
-p_printTree = False # Print the directory tree
+parser = argparse.ArgumentParser(description = 'Checks your Google Drive for NFD encoded filenames and converts them to NFC')
+parser.add_argument('--print_tree', dest='print_tree', action='store_true', help='print the Drive directory tree')
+parser.add_argument('--q_rootfiles', type=str, default="'root' in parents and trashed = false", help='The query string used to get the items to be worked on, default is the root of the drive (q = "%(default)s"). As a precaution, you should try it with a smaller scope, like "title contains \'---accent_test---\'  and trashed = false"')
+parser.add_argument('--no-dry_run', dest='dry_run', action='store_false', help='If set, only then will the NFD->NFC conversion take place')
+parser.set_defaults(dry_run=True)
+parser.set_defaults(print_tree=False)
+args = parser.parse_args()
+print(args)
 
 def report_memory(desc):
     print('Using {} Mb - {}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1000, desc))
@@ -38,7 +42,7 @@ def proc_item(level, ppath, gdfv2):
     nonNfcItems[gdfv2['id']] = ppath + gdfv2['title']
 
   ident_str = '{1:>{0}} '.format(4*level, '')
-  if p_printTree:
+  if args.print_tree:
     print_gfile(ident_str, gdfv2)
 
   if (is_gfolder(gdfv2) and gdfv2['id'] in childList):
@@ -71,7 +75,7 @@ try:
   report_memory('loaded all files')
 
   print("Requesting elements to start with... ", end='', flush=True)
-  rootFileList = drive.ListFile({'q': q_rootfiles}).GetList()
+  rootFileList = drive.ListFile({'q': args.q_rootfiles}).GetList()
   print("done, {0} items\n".format(len(rootFileList)), flush=True)
   report_memory('loaded starting file set')
 
@@ -101,12 +105,15 @@ for file in rootFileList:
   proc_item(0, '/', file)
 report_memory('process of items complete\n')
 
-print('{} item has non-NFC name\n'.format(len(nonNfcItems)))
+print('{} item with non-NFC name\n'.format(len(nonNfcItems)))
 for id,path in nonNfcItems.items():
   gf=fileStash[id]
   dir_str = ' <DIR>' if is_gfolder(gf) else ''
   print('{}{} ... '.format(path,dir_str), end='', flush=True)
 
-  gf['title'] = unicodedata.normalize('NFC', gf['title'])
-  gf.Upload()
-  print('FIXED')
+  if not args.dry_run:
+    gf['title'] = unicodedata.normalize('NFC', gf['title'])
+    gf.Upload()
+    print('FIXED')
+  else:
+    print('NOT CHANGED (dry run)')
