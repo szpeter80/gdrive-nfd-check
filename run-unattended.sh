c6e141812e67895e18568fc64152354f26577dba
szpeter80 <szpeter80@github.com>
2021-12-03 18:36:41 +0100

    ~ Rename script to better reflect function ~ Update unattended runner for name change

--- run-unattended.sh
+++ run-unattended.sh
@@ -4,5 +4,5 @@ LOG_FN=$(date "+report__%Y_%m_%d__%H_%M.txt")
 
 # shellcheck disable=SC1091
 . ./project-venv/bin/activate
-python ./gdrive-nfc-check.py  --print_tree --debug --no-dry_run 2>&1 | tee "./reports/$LOG_FN"
+python ./nfd-checker.py  --print_tree --debug --no-dry_run 2>&1 | tee "./reports/$LOG_FN"
 deactivate
