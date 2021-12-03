a2b57bbda69b70776629191688fe5afb0de770f9
szpeter80 <szpeter80@github.com>
2021-12-03 17:35:35 +0100

    + Added initial bootstrap script for Alpine ~ Modify setup to mention the manual step of editing the settings.yaml

---
+++ setup/alpine-init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apk update
+apk add bash gcc musl-dev libffi-dev git python3 python3-dev
+
+mkdir /mnt/gdrive-nfd-check
+cd /mnt/gdrive-nfd-check
+
+git clone https://github.com/szpeter80/gdrive-nfd-check .
+
+cd setup
+./setup.sh
+
--- setup/setup.sh
+++ setup/setup.sh
@@ -3,3 +3,5 @@ mkdir ../reports
 python3 -m venv ../project-venv
 . ../project-venv/bin/activate
 pip install -r ./requirements.txt
+
+echo "The installation is complete. Don't forget to copy the setup/settings.yaml.example to settings.yaml and edit the app credentials."
